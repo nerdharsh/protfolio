10c6c95259942c5a671c8c1522256393aab463b2
nerdharsh <66509334+nerdharsh@users.noreply.github.com>
2025-06-07 04:44:15 +0530

    Update route.ts

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -3,7 +3,7 @@ import { config } from "@/data/config";
 import { Resend } from "resend";
 import { z } from "zod";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend('re_bJTsNqnF_4rFWoTNe2uETTMvkkLuWczaN');
 
 const Email = z.object({
   fullName: z.string().min(2, "Full name is invalid!"),
